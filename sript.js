c0c014380d7bc71b4c445c541ed56d404e2f35a8
abduvali0626 <umarrrrrrrrrrrrrrr63@gmail.com>
2025-02-16 13:16:51 +0500

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,9 @@
+let ism = prompt(
+  "Name that star:That star locat in scorpius constellation and taht star is old and red supergiant"
+);
+let touchme = document.getElementById("body");
+
+let user = { firstName: ism };
+
+console.log(user);
+
